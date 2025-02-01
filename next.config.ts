7481d8e1f9a450632f36c5dfb49b8e23c8f81c08
priyanshu-101 <priyanshuagarwal1008@gmail.com>
2025-02-01 10:16:56 +0530

    minor fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+const nextConfig = {
+  experimental: {
+    appDir: true,
+    turbo: false, // Disable Turbopack
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
