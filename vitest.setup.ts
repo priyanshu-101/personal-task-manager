c247f016f89bcb69572806ad58f391e6b6ba0930
priyanshu-101 <priyanshuagarwal1008@gmail.com>
2025-07-23 18:29:47 +0530

    minor fixes

---
+++ vitest.setup.ts
@@ -0,0 +1,5 @@
+// vitest.setup.ts
+import '@testing-library/jest-dom'
+
+// You can add global setup configurations here if needed
+// For example, mocking global objects, setting up test environment, etc.
